f22b8593b4cd2abeb9c32a5b802d14cc3c03dc34
Kiran <km060360@gmail.com>
2024-04-27 20:02:10 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { ChangeEvent, useState } from 'react';
+import meanings from './data/data';
+
+const App = () => {
+	const [search, setsearch] = useState('');
+	const [found, setfound] = useState('');
+
+	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setsearch(e.target.value);
+	};
+
+	const handleClick = () => {
+		const allWords = meanings.find((meaning) => {
+			return meaning.word.toLowerCase() == search.toLowerCase();
+		});
+
+		if (allWords) {
+			setfound(allWords?.meaning);
+		} else {
+			setfound('notFound');
+		}
+
+		setsearch('');
+	};
+
+	return (
+		<div>
+			<h1>Dictionary App</h1>
+			<div className='search'>
+				<input
+					type='search'
+					placeholder='Search for a word...'
+					onChange={handleInputChange}
+					value={search}
+				/>
+				<button onClick={handleClick}>Search</button>
+			</div>
+			<h4>Definition:</h4>
+			{found !== '' && (
+				<>
+					{found == 'notFound' ? (
+						'Word not found in the dictionary.'
+					) : (
+						<h2>{found}</h2>
+					)}
+				</>
+			)}
+		</div>
+	);
+};
+
+export default App;
